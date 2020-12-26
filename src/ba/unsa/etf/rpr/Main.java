78084ff90669efde30e4436da88211e73fa27093
jhasanovic <jhasanovic1@etf.unsa.ba>
2020-12-26 03:19:43 +0100

    Dodane boje za ChoiceBox choiceColor

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static javafx.application.Application.launch;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
@@ -13,11 +17,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/novi.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/zadaca2.fxml"));
         primaryStage.setTitle("Studenti");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+
         primaryStage.setMinHeight(291);
         primaryStage.setMinWidth(414);
+
         primaryStage.show();
     }
 
--- src/ba/unsa/etf/rpr/ZadacaController.java
+++ src/ba/unsa/etf/rpr/ZadacaController.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class ZadacaController {
+    public ChoiceBox choiceColor;
+    private ObservableList<String> boje= FXCollections.observableArrayList();
+
+    public ObservableList<String> getBoje() {
+        return boje;
+    }
+
+    public void setBoje(ObservableList<String> boje) {
+        this.boje = boje;
+    }
+
+    @FXML
+    public void initialize() {
+    boje.add("Default");
+    boje.add("Crvena");
+    boje.add("Zelena");
+    boje.add("Plava");
+    choiceColor.setItems(boje);
+    }
 }
